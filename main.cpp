464291a22d414664ff1d6a3341f1b95a2245cc87
Clyde <47860813+Clydeston@users.noreply.github.com>
2023-08-19 18:28:33 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+// these definitions can be changed to any string you wish just updated the parser config
+#define JUNKCODE
+#define JUNKCLASS
+
+JUNKCLASS
+int main()
+{
+  JUNKCODE
+  return 0;
+  JUNKCODE
+}
+JUNKCLASS
